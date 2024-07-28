07ceeb57d734016227a0f55838992020b5c0f1ea
wasan <wasanmohamd.17@gmail.com>
2024-07-28 15:40:41 +0300

    note contoller adding

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@ use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\AuthManager;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\NoteController;
 
 Route::get('/', function () {
     return view('welcome');
